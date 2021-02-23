feef88a5318459645abe98d678e6ce80cc441bfd
Misikiyml <79430340+Misikiyml@users.noreply.github.com>
2021-02-23 15:36:05 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sunxk
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+)
+
+type BookStore struct {
+	XMLName xml.Name `xml:"bookstore"`
+	Books   []Book   `xml:"book"`
+}
+type Book struct {
+	XMLName  xml.Name `xml:"book"`
+	Category string   `xml:"category,attr"`
+	Title    string   `xml:"title"`
+	Author   string   `xml:"author"`
+	Year     string   `xml:"year"`
+	Price    string   `xml:"price"`
+}
+
+func main() {
+	//获取xml文件位置
+	content, err := ioutil.ReadFile("D:\\GoLang\\src\\github.com\\Sunxk\\bookstore.xml")
+	if err != nil {
+		fmt.Println("读文件位置错误信息：", err)
+	}
+
+	//将文件转换为对象
+	var result BookStore
+	err = xml.Unmarshal(content, &result)
+	if err != nil {
+		fmt.Println("读文件内容错误信息：", err)
+	}
+
+	fmt.Println("xml读取:", result)
+	fmt.Println()
+	for _, books := range result.Books {
+		fmt.Println("类别:", books.Category)
+		fmt.Println("题目:", books.Title)
+		fmt.Println("作者:", books.Author)
+		fmt.Println("出版年份:", books.Year)
+		fmt.Println("价格:", books.Price)
+		fmt.Println()
+	}
+
+}
